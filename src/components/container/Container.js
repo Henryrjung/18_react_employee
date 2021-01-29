afdc436c0101106fbfdb8a8aea9ade6b4099ffd1
HenryJ <henryjung667@gmail.com>
2021-01-29 14:40:55 -0600

    added styling to tables

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -3,11 +3,11 @@ import './Container.css'
 
 const Container = ({ children }) => {
   return (
-    <div className='container outer-container'>
-      <label className='name'>Name</label>
-      <label className='email'>Email</label>
-      <label className='number'>Phone Number</label>
-      <div className='container inner-container'>{children}</div>
+    <div className='container'>
+      <label className='employeeName'>Name</label>
+      <label className='employeeEmail'>Email</label>
+      <label className='employeeNumber'>Phone Number</label>
+      <div className='childContainer'>{children}</div>
     </div>
   );
 }
